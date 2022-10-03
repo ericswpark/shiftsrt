0965c44e8720f978bf5e8642b8aee622bc3f1cbd
Eric Park <me@ericswpark.com>
2022-10-03 22:53:25 +0900

    main: return reference to string

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ fn main() {
     }
 }
 
-fn handle_args(args: Vec<String>) -> (String, i32) {
+fn handle_args(args: Vec<String>) -> (&String, i32) {
     // Check if argument count is correct
     const ARG_COUNT: usize = 3;
     if args.len() < ARG_COUNT {
