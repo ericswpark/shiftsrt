4640518abdbb36542d3f403368d740efab2162bd
Eric Park <me@ericswpark.com>
2022-10-03 22:21:53 +0900

    main: write up argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::env;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    // Gather command-line arguments
+    let args: Vec<String> = env::args().collect();
+    let parsed_args = handle_args(args);
+}
+
+fn handle_args(args: Vec<String>) -> (String, i32) {
+    // Check if argument count is correct
+    const ARG_COUNT = 3;
+    if args.len() < ARG_COUNT {
+        panic!("Not enough arguments");
+    } else if args.len > ARG_COUNT {
+        panic!("Too many arguments");
+    }
+
+    // Check if first argument is a valid path and a valid .srt file
+    let path = args[1];
+    if !Path::new(path).exists() {
+        panic!("The first argument must be a valid path. The specified path does not exist.");
+    }
+    if path.len() < 4 || !path.ends_with(".srt") {
+        panic!("The file is not a valid .srt file. Hint: make sure the file extension is correct.");
+    }
+
+    // Check if second argument is time offset in milliseconds
+    let offset: i32 = args[2]
+        .trim()
+        .parse()
+        .expect("Not a valid integer. Input the time offset in milliseconds.");
+
+    (path, offset)
 }
