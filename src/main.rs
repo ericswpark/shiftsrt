0c6f838e9aa57ceb0910d0bd19125e5f08ae4d7d
Eric Park <me@ericswpark.com>
2022-10-05 10:52:59 +0900

    main: fix path fetching function
    Now fetches target file path too

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,14 @@ const TIME_LINE_FORMAT_REGEX: &str = r"\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\
 
 fn main() {
     // Gather command-line arguments
-    let (path, offset) = handle_args(env::args().collect());
+    let (source_path, target_path, offset) = handle_args(env::args().collect());
 
-    println!("Shifting file {} with offset {}.", path, offset);
+    println!("Shifting file {} with offset {}.", source_path, offset);
 
     // Get source file to read
-    let source_file = File::open(path).expect("Failed to open source file.");
+    let source_file = File::open(source_path).expect("Failed to open source file.");
     let source_file_reader = BufReader::new(source_file);
+    let mut target_file = File::create(target_path).expect("Failed to open target file.");
 
     let mut is_time_line = false;
     let regex_matcher = Regex::new(TIME_LINE_FORMAT_REGEX).unwrap();
@@ -24,7 +25,7 @@ fn main() {
     }
 }
 
-fn handle_args(args: Vec<String>) -> (String, i32) {
+fn handle_args(args: Vec<String>) -> (String, String, i32) {
     // Check if argument count is correct
     const ARG_COUNT: usize = 3;
     if args.len() < ARG_COUNT {
@@ -34,11 +35,11 @@ fn handle_args(args: Vec<String>) -> (String, i32) {
     }
 
     // Check if first argument is a valid path and a valid .srt file
-    let path = &args[1];
-    if !Path::new(&path).exists() {
+    let source_path = &args[1];
+    if !Path::new(&source_path).exists() {
         panic!("The first argument must be a valid path. The specified path does not exist.");
     }
-    if path.len() < 4 || !path.ends_with(".srt") {
+    if source_path.len() < 4 || !source_path.ends_with(".srt") {
         panic!("The file is not a valid .srt file. Hint: make sure the file extension is correct.");
     }
 
@@ -48,5 +49,5 @@ fn handle_args(args: Vec<String>) -> (String, i32) {
         .parse()
         .expect("Not a valid integer. Input the time offset in milliseconds.");
 
-    (path.clone(), offset)
+    (source_path.clone(), target_path, offset)
 }
