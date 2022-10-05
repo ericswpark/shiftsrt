dcb24e40fdc07e920a86948c49d1acbcac9606f3
Eric Park <me@ericswpark.com>
2022-10-05 10:53:57 +0900

    main: implement the rest of the logic

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,19 @@ fn main() {
     let regex_matcher = Regex::new(TIME_LINE_FORMAT_REGEX).unwrap();
 
     for line in source_file_reader.lines() {
-        // TODO Implement the rest of the logic here
+        let line = line.unwrap();
+        if !regex_matcher.is_match(&line) {
+            // Not a time line, write directly to file
+            writeln!(&mut target_file, "{}", line).unwrap();
+        } else {
+            let times: Vec<&str> = line.split(" --> ").collect();
+            let start_time = times[0];
+            let end_time = times[1];
+
+            let start_time = shift(start_time.to_string(), offset);
+            let end_time = shift(end_time.to_string(), offset);
+            writeln!(&mut target_file, "{} --> {}", start_time, end_time).unwrap();
+        }
     }
 }
 
@@ -50,4 +62,31 @@ fn handle_args(args: Vec<String>) -> (String, String, i32) {
         .expect("Not a valid integer. Input the time offset in milliseconds.");
 
     (source_path.clone(), target_path, offset)
+}
+
+fn shift(time: String, offset: i32) -> String {
+    let parts: Vec<&str> = time.split(",").collect();
+    let millisecond: u16 = parts[1].trim().parse().unwrap();
+
+    let parts: Vec<&str> = parts[0].split(":").collect();
+    let hour: u8 = parts[0].trim().parse().unwrap();
+    let minute: u8 = parts[1].trim().parse().unwrap();
+    let second: u8 = parts[2].trim().parse().unwrap();
+
+    println!("Hour: {}, Minute: {}, Second: {}", hour, minute, second);
+    let second: i64 = (i64::from(second) + i64::from(minute) * 60 + i64::from(hour) * 60 * 60).into();
+    let millisecond: i64 = (i64::from(millisecond) + second * 1000).into();
+
+    let mut millisecond = millisecond + i64::from(offset);
+
+    println!("Millisecond: {}", millisecond);
+    let hour: u8 = (millisecond / (1000 * 60 * 60)).try_into().unwrap();
+    millisecond -= millisecond / (1000 * 60 * 60);
+    let minute: u8 = (millisecond / (1000 * 60)).try_into().unwrap();
+    millisecond -= millisecond / (1000 * 60);
+    println!("Second divided: {}", (millisecond / 1000));
+    let second: u8 = (millisecond / 1000).try_into().unwrap();
+    millisecond -= millisecond / 1000;
+
+    format!("{}:{}:{},{}", hour, minute, second, millisecond)
 }
